bbbc9086678d404e93002ca3a90fce4967a915b9
Afzal14786 <mdafzal14777@gmail.com>
2026-07-17 11:42:52 +0530

    Practice 1475A

---
+++ 900/1475A_A_Odd_Divisor/solution.cpp
@@ -0,0 +1,22 @@
+// رَبِّ زِدْنِي عِلْمًا
+// اے میرے رب! میرے علم میں اضافہ فرما۔
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    long long n;
+    cin >> n;
+    if ((n & (n-1)) == 0) {
+        cout << "No" << endl;
+    } else {
+        cout << "Yes" << endl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t; cin >> t;
+    while (t--) solve();
+    return 0;
+}
