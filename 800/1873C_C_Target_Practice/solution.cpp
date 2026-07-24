457b3a3c973fad050479da7f7deb9709f46fc4b5
Afzal14786 <mdafzal14777@gmail.com>
2026-07-24 20:06:53 +0530

    Practice 1873 C Target Practice

---
+++ 800/1873C_C_Target_Practice/solution.cpp
@@ -0,0 +1,49 @@
+// رَبِّ زِدْنِي عِلْمًا
+// اے میرے رب! میرے علم میں اضافہ فرما۔
+#include <bits/stdc++.h>
+using namespace std;
+
+const int score[10][10] = {
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,2,2,2,2,2,2,2,2,1},
+    {1,2,3,3,3,3,3,3,2,1},
+    {1,2,3,4,4,4,4,3,2,1},
+    {1,2,3,4,5,5,4,3,2,1},
+    {1,2,3,4,5,5,4,3,2,1},
+    {1,2,3,4,4,4,4,3,2,1},
+    {1,2,3,3,3,3,3,3,2,1},
+    {1,2,2,2,2,2,2,2,2,1},
+    {1,1,1,1,1,1,1,1,1,1},
+    
+};
+
+
+void solve() {
+    char grid[10][10];
+
+    
+    for (int i = 0; i < 10; ++i) {
+        string str;
+        cin >> str;
+        for (int j = 0; j < 10; ++j) {
+            grid[i][j] = str[j];
+        }
+    }
+
+    int total_score = 0;
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            if (grid[i][j] == 'X') total_score += score[i][j];
+        }
+    }
+
+    cout << total_score << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t; cin >> t;
+    while (t--) solve();
+    return 0;
+}
